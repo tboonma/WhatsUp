2dca5ac3678a7284fb743b6ad07e682f3fd60c40
Tawan Boonma <tboonma@hotmail.com>
2024-02-12 14:50:04 +0700

    Finished class 03

---
+++ app/src/main/java/org/classapp/whatsup/LogInViewActivity.kt
@@ -0,0 +1,11 @@
+package org.classapp.whatsup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in_view)
+    }
+}
